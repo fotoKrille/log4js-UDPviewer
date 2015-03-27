29a7d3576c0fcc4321f9970f5f1367f27087d92e
Krister Johansson <info@fotokrille.se>
2015-03-27 14:31:09 +0100

    First push

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # log4js-UDPviewer
-A simpel application written in NODE JS to display incomming UDP message from log4js
+A simpel application written in NODE JS to display incoming UDP message from log4js
---
+++ server.js
@@ -0,0 +1,20 @@
+var dgram = require("dgram"),
+	server = dgram.createSocket("udp4");
+
+var log4js = require("log4js"),
+	logger = log4js.getLogger();
+
+var serverAddress = process.env.address || "127.0.0.1",
+	serverPort = process.env.port || 80;
+
+server.on("listening", function () {
+    var address = server.address();
+    logger.info("log4js listening on " + address.address + ":" + address.port);
+});
+
+server.on("message", function (message, remote) {
+	message = JSON.parse(message);
+	logger[message.fields.level.toLowerCase()](message.message);
+});
+
+server.bind(80);
