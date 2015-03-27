781eb07d0bc5a94cd61d41b52f02ed21d175647f
Krister Johansson <info@fotokrille.se>
2015-03-27 14:32:16 +0100

    Spaces

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var dgram = require("dgram"),
-	server = dgram.createSocket("udp4");
+    server = dgram.createSocket("udp4");
 
 var log4js = require("log4js"),
-	logger = log4js.getLogger();
+    logger = log4js.getLogger();
 
 var serverAddress = process.env.address || "127.0.0.1",
-	serverPort = process.env.port || 80;
+    serverPort = process.env.port || 80;
 
 server.on("listening", function () {
     var address = server.address();
@@ -13,8 +13,8 @@ server.on("listening", function () {
 });
 
 server.on("message", function (message, remote) {
-	message = JSON.parse(message);
-	logger[message.fields.level.toLowerCase()](message.message);
+    message = JSON.parse(message);
+    logger[message.fields.level.toLowerCase()](message.message);
 });
 
 server.bind(80);
